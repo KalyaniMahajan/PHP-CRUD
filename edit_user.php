4fbd13dff9293b73dc3c391252f42d6b66833d04
Kalyani Mahajan <59738224+KalyaniMahajan@users.noreply.github.com>
2020-04-12 05:27:51 +0530

    create add,delete,update features and added database file

---
+++ edit_user.php
@@ -0,0 +1,54 @@
+<?php
+	require_once("config.php");
+	if(count($_POST)>0) {
+		$sql = "UPDATE users set userName='" . $_POST["userName"] . "', password='" . $_POST["password"] . "', firstName='" . $_POST["firstName"] . "', lastName='" . $_POST["lastName"] . "' WHERE userId='" . $_POST["userId"] . "'";
+		mysqli_query($conn,$sql);
+		$message = "Record Modified Successfully";
+		header("location: index.php");
+	}
+	$select_query = "SELECT * FROM users WHERE userId='" . $_GET["userId"] . "'";
+	$result = mysqli_query($conn,$select_query);
+	$row = mysqli_fetch_array($result);
+?>
+<html>
+	<head>
+		<title>Add New User</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<form name="frmUser" method="post" action="">
+			<div class="container">
+				<div class="message"><?php if(isset($message)) { echo $message; } ?></div>
+				<div align="right" style="padding-bottom:5px;">
+					<a href="index.php" class="link"><img alt='List' title='List' src='images/list.png' width='15px' height='15px'/> List User</a>
+				</div>
+				<table border="0" cellpadding="10" cellspacing="0" align="center" class="tblSaveForm container">
+					<tr class="tableheader">
+						<td colspan="2">Edit User</td>
+					</tr>
+					<tr>
+						<td><label>Username</label></td>
+						<td>
+							<input type="hidden" name="userId" class="txtField" value="<?php echo $row['userId']; ?>">
+							<input type="text" name="userName" required="" class="txtField" value="<?php echo $row['userName']; ?>">
+						</td>
+					</tr>
+					<tr>
+						<td><label>Password</label></td>
+						<td><input type="password" name="password" required=""class="txtField" value="<?php echo $row['password']; ?>"></td>
+					</tr>
+					<tr>
+						<td><label>First Name</label></td>
+						<td><input type="text" name="firstName" required="" class="txtField" value="<?php echo $row['firstName']; ?>"></td>
+					</tr>
+						<td><label>Last Name</label></td>
+						<td><input type="text" name="lastName" required="" class="txtField" value="<?php echo $row['lastName']; ?>"></td>
+					</tr>
+					<tr>
+						<td colspan="2"><input type="submit" name="submit" value="Submit" class="btnSubmit"></td>
+					</tr>
+				</table>
+			</div>
+		</form>
+	</body>
+</html>
